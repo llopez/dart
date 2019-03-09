625a5d61a52c308d7b5d956ecc418b200cf1b7e0
Luis Lopez <luigibyte@gmail.com>
2019-03-09 16:36:58 -0300

    Seed todos

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,7 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+Dart.Repo.insert!(%Dart.Todos.Todo{title: "Clean the house", completed: false})
+Dart.Repo.insert!(%Dart.Todos.Todo{title: "Fly the drone", completed: false})
+Dart.Repo.insert!(%Dart.Todos.Todo{title: "Play a game", completed: false})
