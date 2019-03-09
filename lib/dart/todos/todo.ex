3f13b83095f226ef58a3405f7ae3421a6cd59db8
Luis Lopez <luigibyte@gmail.com>
2019-03-09 15:58:23 -0300

    Generate default json structure for todos in context Todos

---
+++ lib/dart/todos/todo.ex
@@ -0,0 +1,19 @@
+defmodule Dart.Todos.Todo do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+
+  schema "todos" do
+    field :completed, :boolean, default: false
+    field :title, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(todo, attrs) do
+    todo
+    |> cast(attrs, [:title, :completed])
+    |> validate_required([:title, :completed])
+  end
+end
